e163b46c9172582eeca133017ef7dbe774a40641
kanza.syed <kanza.syed@pral.com.pk>
2021-01-15 14:11:05 +0500

    Seed Data

---
+++ src/psw.itms.service/Strategies/MockData/SeedData.cs
@@ -0,0 +1,15 @@
+namespace PSW.ITMS.Service.Strategies
+{
+    public class SeedData
+    {
+        public int AgencyId {get;set;}
+        public string HSCode { get; set; }
+        public string CommodityName { get; set; }
+        public string Purpose { get; set; }
+
+        public string IPDocumentaryRequirements { get; set; }
+        public string IPFees { get; set; }
+        public string TechnicalName { get; set; }
+
+    }
+}
