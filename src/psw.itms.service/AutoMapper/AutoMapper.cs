bad920bc368a4af4a824262d951187c922c2c4b8
kanza.syed <kanza.syed@pral.com.pk>
2021-01-15 14:08:47 +0500

    Unnecessary Comments Removed

--- src/psw.itms.service/AutoMapper/AutoMapper.cs
+++ src/psw.itms.service/AutoMapper/AutoMapper.cs
@@ -2,10 +2,8 @@ using AutoMapper;
 using PSW.ITMS.Data.Entities;
 using PSW.ITMS.Service.DTO;
 
-
 namespace PSW.ITMS.Service.Mapper
 {
-
     public class ObjectMapper
     {
         public IMapper _mapper { get; set; }
@@ -25,13 +23,10 @@ namespace PSW.ITMS.Service.Mapper
             try
             {
                 // Place All Mappings Here
-                var config = new MapperConfiguration(cfg =>
-                {
-                    //cfg.CreateMap<CountryWithDialingCodes, GetAllCountryCodesResponseDTO>();
-                    // cfg.CreateMap<Country, GetAllCountriesResponseDTO>();
-                    // cfg.CreateMap<PCTCode, GetHSCodeListResponseDTO>();
-                    //cfg.CreateMap<TransportMeans, GetMeanOfTransportResponseDTO>();
-                });
+                var config =
+                    new MapperConfiguration(cfg =>
+                        {
+                        });
                 _mapper = config.CreateMapper();
             }
             catch
@@ -39,6 +34,5 @@ namespace PSW.ITMS.Service.Mapper
                 throw;
             }
         }
-
     }
 }
