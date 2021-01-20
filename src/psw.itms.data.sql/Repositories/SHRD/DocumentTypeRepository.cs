518e98a387490576edf6fd56cdb7cae3a39d471d
kanza.syed <kanza.syed@pral.com.pk>
2021-01-20 11:53:40 +0500

    Prefix of SHRD DB added

--- src/psw.itms.data.sql/Repositories/SHRD/DocumentTypeRepository.cs
+++ src/psw.itms.data.sql/Repositories/SHRD/DocumentTypeRepository.cs
@@ -14,7 +14,7 @@ namespace PSW.ITMS.Data.Sql.Repositories
 
         public DocumentTypeRepository(IDbConnection context) : base(context)
         {
-            TableName = "[dbo].[DocumentType]";
+            TableName = "[SHRD].[dbo].[DocumentType]";
 			PrimaryKeyName = "Code";
         }
 
