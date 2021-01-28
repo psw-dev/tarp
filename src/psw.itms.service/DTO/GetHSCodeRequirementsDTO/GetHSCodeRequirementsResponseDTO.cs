00c70a7de7172274ba5341f12e2721d177465a46
kanza.syed <kanza.syed@pral.com.pk>
2021-01-28 12:56:20 +0500

    Response DTOs

--- src/psw.itms.service/DTO/GetHSCodeRequirementsDTO/GetHSCodeRequirementsResponseDTO.cs
+++ src/psw.itms.service/DTO/GetHSCodeRequirementsDTO/GetHSCodeRequirementsResponseDTO.cs
@@ -1,22 +1,30 @@
 using System;
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PSW.ITMS.Service.DTO
 {
     public class GetHSCodeRequirementsResponseDTO
     {
         
-        [JsonPropertyName("agencyId")]
-        public string PurposeIDList { get; set; }
+        // [JsonPropertyName("purposeIDList")]
+        // public string PurposeIDList { get; set; }
 
-        [JsonPropertyName("technicalName")]
-        public string TechnicalName { get; set; }
+        // [JsonPropertyName("technicalName")]
+        // public string TechnicalName { get; set; }
 
-        [JsonPropertyName("itemDescription")]
-        public string ItemDescription { get; set; }
+        // [JsonPropertyName("itemDescription")]
+        // public string ItemDescription { get; set; }
 
-        [JsonPropertyName("UoMId")]
-        public int UoMID { get; set; }
+        // [JsonPropertyName("UoMId")]
+        // public int UoMID { get; set; }
+        
+        public IList<GetPurposeOfImportByHSCodeResponseDTO> purposesOfImport { get; set; }
+        public IList<DocumentResponseDTO> documents { get; set; }
+
+        public IList<UOMResponseDTO> UoMs { get; set; }
+        public IList<ListOfRules> rules { get; set; }
     }
 
 }
