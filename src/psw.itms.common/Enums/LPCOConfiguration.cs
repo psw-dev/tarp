ac14c2ff048d01a04a82da503400f54275d2a4f6
muhammad.kamran <muhammad.kamran@psw.gov.pk>
2022-05-17 10:18:45 +0500

    LPCOConfiguration Enum

--- src/psw.itms.common/Enums/LPCOConfiguration.cs
+++ src/psw.itms.common/Enums/LPCOConfiguration.cs
@@ -2,7 +2,7 @@ namespace PSW.ITMS.Common.Enums
 {
     public enum LPCOConfiguration
     {
-        IncreasedRate = 25, //
+        IncreasedRate = 25,  
         PerCageRate = 25,
         CageUoM = 100003
     }
