b2073d1a5006bbfc0eaa85fe44b607a2b7355d40
maryam.abid <maryam.abid@psw.gov.pk>
2022-12-01 11:20:43 +0500

    Country ListF rom Tarp

---
+++ src/psw.itms.service/Strategies/GetRequirementList/GetCountryListStrategy.cs
@@ -0,0 +1,171 @@
+using MongoDB.Bson;
+using psw.security.Encryption;
+using PSW.ITMS.Common.Enums;
+using PSW.ITMS.Common.Model;
+using PSW.ITMS.Data.Entities;
+using PSW.ITMS.service;
+using PSW.ITMS.Service.Command;
+using PSW.ITMS.Service.DTO;
+using PSW.ITMS.Service.MongoDB;
+using PSW.Lib.Logs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PSW.ITMS.Service.Strategies
+{
+    public class GetCountryListStrategy : ApiStrategy<GetCountryListRequest, GetCountryListResponse>
+    {
+        #region Constructors 
+        public GetCountryListStrategy(CommandRequest request) : base(request)
+        {
+            Reply = new CommandReply();
+        }
+        #endregion 
+
+        #region Distructors 
+        ~GetCountryListStrategy()
+        {
+
+        }
+        #endregion 
+
+        #region Strategy Excecution  
+
+        public override CommandReply Execute()
+        {
+            try
+            {
+               
+
+                Log.Information("|{0}|{1}| Request DTO {@RequestDTO}", StrategyName, MethodID, RequestDTO);
+
+                RegulatedHSCode tempHsCode = Command.UnitOfWork.RegulatedHSCodeRepository.GetActiveHsCode(RequestDTO.HsCode, RequestDTO.AgencyId, RequestDTO.TradeTranTypeID, RequestDTO.documentTypeCode);
+
+                // Log.Information("|{0}|{1}| RegulatedHSCode DbRecord {@tempHsCode}", StrategyName, MethodID, tempHsCode);
+
+                // if (tempHsCode == null)
+                // {
+                //     return BadRequestReply("Record for hscode does not exist");
+                // }
+
+                // var tempRule = Command.UnitOfWork.RuleRepository.Get(Convert.ToInt16(tempHsCode.RuleID));
+
+                // if (tempRule == null)
+                // {
+                //     return BadRequestReply("Record for rule against hscode does not exist");
+                // }
+
+
+                var mongoDoc = new BsonDocument();
+                MongoDbRecordFetcher mongoDBRecordFetcher;
+
+                // try
+                // {
+                    mongoDBRecordFetcher = new MongoDbRecordFetcher("TARP", tempHsCode.CollectionName, Environment.GetEnvironmentVariable("MONGODBConnString"));
+              //  }
+                // catch (SystemException ex)
+                // {
+                //     Log.Error("|{0}|{1}| Error occured in connecting to MongoDB {@ex}", StrategyName, MethodID, ex.ToString());
+
+                //     return BadRequestReply("Error occured in connecting to MongoDB");
+                // }
+
+                try
+                {
+                   //here is my change
+                   if(mongoDBRecordFetcher!=null)
+                       { mongoDoc = mongoDBRecordFetcher.GetFilteredRecordMFD(RequestDTO.HsCode);
+                        if (mongoDoc == null)
+                        {
+                            return BadRequestReply(String.Format("No record found for HsCode : {0}", RequestDTO.HsCode));
+                        }
+                       }
+                  
+                   
+                 }
+                catch (SystemException ex)
+                {
+                    Log.Error("|{0}|{1}| Error occured in fetching record from MongoDB {@ex}", StrategyName, MethodID, ex.ToString());
+
+                    return BadRequestReply("Error occured in fetching record from MongoDB");
+                }
+
+
+
+                Log.Information("|{0}|{1}| Mongo Record fetched {@mongoDoc}", StrategyName, MethodID, mongoDoc);
+
+
+                var docType = this.Command.UnitOfWork.DocumentTypeRepository.Where(new
+                { Code = RequestDTO.documentTypeCode }).FirstOrDefault();
+
+              var response = GetRequirements(mongoDoc, docType.DocumentClassificationCode);
+                Log.Information("|{0}|{1}| Required LPCO Parent Code {@documentClassification}", StrategyName, MethodID, docType.DocumentClassificationCode);
+
+               ResponseDTO = response;
+
+
+                Log.Information("|{0}|{1}| LPCO required {2}", StrategyName, MethodID, "true");
+
+                Log.Information("|{0}|{1}| Response {@ResponseDTO}", StrategyName, MethodID, ResponseDTO);
+
+
+                // Send Command Reply 
+                return OKReply();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("|{0}|{1}| Exception Occurred {@ex}", StrategyName, MethodID, ex);
+                return InternalServerErrorReply(ex);
+            }
+        }
+        #endregion
+
+       
+        public GetCountryListResponse GetRequirements(BsonDocument mongoRecord, string documentClassification)
+        {
+            Log.Information("[{0}.{1}] Started", GetType().Name, MethodBase.GetCurrentMethod().Name);
+            Log.Information("|{0}|{1}| documentClassification {documentClassification}", StrategyName, MethodID, documentClassification);
+            var response = new GetCountryListResponse();
+
+    
+            
+            Log.Information("|{0}|{1}| documentClassification {documentClassification}", StrategyName, MethodID, documentClassification);
+
+                var healthCertificateFeeRequired = false;
+                var countries = new List<string>();
+
+            
+            
+
+                        string ECFeeString = mongoRecord["Certificate of Quality and Origin Processing Fee (PKR)"].ToString();
+                        Log.Information("|{0}|{1}| ECFeeString {ECFeeString}", StrategyName, MethodID, ECFeeString);
+
+                
+
+                        // The column that tells if Health Certificate is Fee Required (Conditional)
+                        // Condition: If the destination country is from one of the countries in the following column, then fee is applied.
+                        // "Names of Countries Requiring Health Certificate on prescribed format"
+                        countries = mongoRecord["Codes of Countries Requiring Health Certificate on prescribed format"].ToString().Split('|').ToList();
+                        if (countries.Count > 0)
+                        {
+                            countries = countries.Select(t => t.Trim()).ToList();
+                            countries = countries.Select(t => t.Replace("\n", "").Replace("\r", "")).ToList();
+                        }
+                        Log.Information("|{0}|{1}| countries {@countries}", StrategyName, MethodID, countries);
+                        Log.Information("|{0}|{1}| RequestDTO.DestinationCountryCode {RequestDTO.DestinationCountryCode}", StrategyName, MethodID, RequestDTO.DestinationCountryCode);
+
+                        if (countries.Contains(RequestDTO.DestinationCountryCode))
+                        {
+                            response.isPrintAllowed = true; // use later 
+
+                            
+                        }
+
+           // Log.Information("Tarp Requirments Response: {@response}", response);
+            Log.Information("[{0}.{1}] Ended", GetType().Name, MethodBase.GetCurrentMethod().Name);
+            return response;
+        }
+    }
+}
