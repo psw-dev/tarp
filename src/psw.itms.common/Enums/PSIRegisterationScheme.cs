25966195e75ccb32c147b310f5c248f60acde538
saad.rashid <saad.rashid@pral.com.pk>
2023-01-24 15:33:51 +0500

    PSW-46542 : PSI Registeration Fixes

---
+++ src/psw.itms.common/Enums/PSIRegisterationScheme.cs
@@ -0,0 +1,12 @@
+﻿namespace psw.itms.common.Enums
+{
+    public struct PSIRegisterationScheme
+    {
+        public const string Form1 = "form 1";
+        public const string Form16 = "form 16";
+        public const string Form17 = "form 17";
+        public const string Form1_Certificate_DocType = "F20";
+        public const string Form16_Certificate_DocType = "F22";
+        public const string Form17_Certificate_DocType = "F24";
+    }
+}
